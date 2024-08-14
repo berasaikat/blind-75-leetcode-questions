2c4cf603d0bdeaa2d035fcc24e26af25b7cd80ec
Saikat Bera <saikatbera2001@gmail.com>
2024-08-14 23:26:34 +0530

    Added solution for questions 73 and 75

---
+++ 73_word_search_II.py
@@ -0,0 +1,45 @@
+class TrieNode:
+    def __init__(self) -> None:
+        self.children = {}
+        self.isWord = False
+    def addWord(self, word):
+        current = self
+        for letter in word:
+            if letter not in current.children:
+                current.children[letter] = TrieNode()
+            current = current.children[letter]
+        current.isWord = True
+
+class Solution(object):
+    def findWords(self, board, words):
+        """
+        :type board: List[List[str]]
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        root = TrieNode()
+        for letter in words:
+            root.addWord(letter)
+        rows, cols = len(board), len(board[0])
+        result, visit = set(), set()
+        def dfs(r, c, node, word):
+            if (r < 0 or c < 0 or
+                r == rows or c == cols or
+                board[r][c] not in node.children or
+                (r, c) in visit):
+                return
+            visit.add((r, c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.isWord:
+                result.add(word)
+            dfs(r + 1, c, node, word)
+            dfs(r - 1, c, node, word)
+            dfs(r, c + 1, node, word)
+            dfs(r, c - 1, node, word)
+            visit.remove((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                dfs(r, c, root, "")
+        return list(result)
